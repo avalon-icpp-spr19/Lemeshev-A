15937e56e14458b86c9331f880ef4a25fa3d71a8
Ukrocoder <47276651+Ukrocoder@users.noreply.github.com>
2019-03-15 22:41:29 +0300

    Доп. задание не делал, остальное по ГОСТу. Честно, я забыл про это задание.

--- While_For_Lemeshev.cpp
+++ While_For_Lemeshev.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 #include <limits>
+#include <string>
+void space(int n, char c)
+{
+	/*Несмотря на то, что первым циклом мы даём 0, в процедуре алгоритм перд проверкой выполняется хотя бы раз,
+	поэтому и вырисовывается один символ, а дальше в соответвии с данными. 
+	Подпрограммы - наше всё)*/
+	for (int i = 0; i <= n; ++i)
+	{
+		std::cout << c;
+	}
+}
 void while_part()
 {
 	/*int min = std::numeric_limits<int>::max(), max = std::numeric_limits<int>::min();
@@ -69,14 +80,88 @@ void while_part()
 		b = x;
 		++i;
 	}
-	std::cout <<x << std::endl;
+	std::cout <<x << std::endl;*/
 }
 /*====================================================================================================*/
+void for_part()
+{
+	/*    int x;
+		int c;
+		c = 0;
+		std::cout << "Задание первое, введите число от 1 до 27 и узнайте, сумма цифр скольких" << std::endl << "трёхзначных чисел ему равна." << std::endl;
+		std::cin >> x;
+		for (int i = 100; i <= 999; ++i)
+		{
+			if ((i / 100) + (i % 100 / 10) + (i % 10) == x) { c++; };//c++, he-he)
+		}
+	   std::cout << c << std::endl;
+/*====================================================================================================*/
+/*std::string line;
+std::cout << "Задание второе, введите строку и получите её же в рамке." << std::endl;
+std::getline(std::cin, line);
+int size = line.size()+1;
+std::cout << "+";
+for (int i = 0; i <= size; i++)
+{
+	std::cout << "-";
+}
+std::cout << "+"<<std::endl<<"| "<< line<<" |"<<std::endl<<"+";
+for (int i = 0; i <= size; i++)
+{
+	std::cout << "-";
+}
+std::cout << "+";
+/*====================================================================================================*/
+/*int x;
+std::cout << "Задание третье, введите число и получите шахматную доску с данной стороной." << std::endl;
+std::cin >> x;
+for (int i = 0; i <= x; ++i)
+{
+	for (int e = 0; e <= x; ++e)
+	{
+		e += i % 2;//Проверка чётности-нечётности выводимой строки.
+		if (e % 2 == 0)
+		{
+			std::cout << "#";
+		}
+		else
+		{
+			std::cout << "*";
+		}
+		e -= i % 2;//Возврат у исходнику для поддержания заданой структуры
+	}
+	std::cout << std::endl;
+}
+std::cin >> x;
+/*====================================================================================================*/
+	/*int x;
+	std::cout << "Задание четвёртое, введите число и получите шахматную ромб с данной стороной." << std::endl;
+	std::cin >> x;
+	for (int i = 0; i < x; ++i)
+	{
+		space(x - i, ' ');
+		space(i * 2, 'X');
+		space(x - i, ' ');
+		std::cout << std::endl;
+	}
+	for (int i = x; i >= 0; --i)
+	{
+		space(x-i, ' ');
+		space(i * 2, 'X');
+		space(x-i, ' ');
+		std::cout << std::endl;
+	}
+	std::cout << "Если насмотрелись, введите ноль, чтобы выйти из прлграммы." << std::endl;
+	std::cin >> x;
+	/*====================================================================================================*/
+}
 int main()
 /*Ivan Andreevich, don't forget to remove comment marks before using!
 Remember: only one task can be activated at the same time, or you'll recive an error message of type "multiple initializing."*/
-{
+    {
 	setlocale(0, "");
 	/*Сектор while*/
 	while_part();
+	/*Сектор For*/
+	for_part();
 	}
